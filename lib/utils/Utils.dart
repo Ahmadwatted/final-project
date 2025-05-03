95196404e492a163508c663ea57dfa3d0cd2f3cc
Ahmad <ahmad2015watted@gmail.com>
2025-05-03 22:04:31 +0300

    last commit and push probably

--- lib/utils/Utils.dart
+++ lib/utils/Utils.dart
@@ -7,36 +7,12 @@ import 'package:flutter/material.dart';
 
 class Utils{
 
-  // static void alert(String _txtEmail,String _txtPassword, BuildContext context, String _txtFirstName,String _txtSecondName) {
-  //
-  //       showDialog<String>(
-  //         context: context,
-  //         builder: (BuildContext context) =>
-  //             AlertDialog(
-  //               title:  Text('$_txtFirstName,$_txtSecondName are u sure about this info?'),
-  //               content:  Text('your Email:$_txtEmail,\n your Password:$_txtPassword ',),
-  //
-  //
-  //               actions: <Widget>[
-  //                 TextButton(
-  //                   onPressed: () => Navigator.push(context,MaterialPageRoute(builder: (context) => MyHomePage(title: 'asdasd',))),
-  //                   child: const Text('Cancel'),
-  //                 ),
-  //                 TextButton(
-  //                   onPressed: () => Navigator.pop(context, 'Yes'),
-  //                   child: const Text('OK'),
-  //                 ),
-  //               ],
-  //             ),
-  //       );
-  //
-  // }
 
 
   Future<void> showMyDialog(User us, BuildContext context) async {
     return showDialog<void>(
       context: context,
-      barrierDismissible: false, // user must tap button!
+      barrierDismissible: false,
       builder: (BuildContext context) {
         return AlertDialog(
           title:  Text('$us._txtfirstName,$us._txtsecondName are u sure about the info you have  entered'),
@@ -77,7 +53,7 @@ class Utils{
   Future<void> showMyDialog2(BuildContext context, String title, String content ) async {
     return showDialog<void>(
       context: context,
-      barrierDismissible: false, // user must tap button!
+      barrierDismissible: false,
       builder: (BuildContext context) {
         return AlertDialog(
           title:  Text(title),
